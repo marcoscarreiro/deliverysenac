c764492c7897ebd4a72a28176d9e3e1a8a3d50fc
marcoscarreiro <marcosviniciusc.silva@yahoo.com.br>
2025-06-27 12:10:34 -0300

    Adicionado funcionalidades Carrinho

---
+++ pizzas.js
@@ -0,0 +1,26 @@
+let cardapio = [
+    {
+        foto: "assets/img/Pizza4.jpg",
+        nome: "Calabresa",
+        ingredientes: ["Calabresa", "Cebola", "Muçarela"],
+        valor: 35.00
+    },
+    {
+        foto: "assets/img/Pizza2.jpg",
+        nome: "Muçarela",
+        ingredientes: ["Beringela", "Cebola", "Muçarela"],
+        valor: 40.00
+    },
+    {
+        foto: "assets/img/Pizza6.jpg",
+        nome: "Mista de Vegetais",
+        ingredientes: ["Beringela", "Abobrinha", "Pimentão", "Cebola", "Muçarela"],
+        valor: 40.00
+    },
+    {
+        foto: "assets/img/FCatupiry.jpg",
+        nome: "Frango com Catupiry",
+        ingredientes: ["Frango", "Catupiry", "Muçarela"],
+        valor: 40.00
+    }
+];
